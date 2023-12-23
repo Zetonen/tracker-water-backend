9d6edb13d13f90f93a5a5e459c879fea64d5db80
Seven <graf.lancelot7@gmail.com>
2023-12-23 18:00:06 +0200

    add proj

---
+++ controllers/waterTrack/updateWaterById.js
@@ -0,0 +1,20 @@
+import dateFormat from "../../helpers/dateFormat.js";
+import WaterTrack from "../../model/WaterTracker.js";
+
+const updateWaterById = async (req, res) => {
+  const { waterId } = req.params;
+  const { _id: owner } = req.user;
+  const waterTrack = { ...req.body };
+  if (waterTrack.date) {
+    waterTrack.date = dateFormat(waterTrack.date);
+  }
+  const result = await WaterTrack.findOneAndUpdate(
+    { _id: waterId, owner },
+    waterTrack
+  );
+  if (!result) {
+    throw HttpError(404, `Contact with id = ${waterId} is found`);
+  }
+  res.json(result);
+};
+export default updateWaterById;
--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,4 @@
 export { default as HttpError } from "./HttpError.js";
 export { default as paramsAvatar } from "./paramsAvatar.js";
 export { default as sendEmail } from "./sendEmail.js";
+export { default as dateFormat } from "./dateFormat.js";
