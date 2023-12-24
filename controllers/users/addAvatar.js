080329c2c71c87ff9c0c0101dd71608f1149d2cd
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-12-24 13:53:46 +0200

    added functional with cloudinary

--- controllers/users/addAvatar.js
+++ controllers/users/addAvatar.js
@@ -1,9 +1,7 @@
 import fs from "fs/promises";
 import path from "path";
-import { HttpError, paramsAvatar } from "../../helpers/index.js";
+import { HttpError, paramsAvatar, cloudinary } from "../../helpers/index.js";
 import User from "../../model/User.js";
-
-const avatarPath = path.resolve("public", "avatars");
 
 const addAvatar = async (req, res, next) => {
   const { _id: id } = req.user;
@@ -12,14 +10,12 @@ const addAvatar = async (req, res, next) => {
   }
   const { path: oldPath, originalname } = req.file;
   const filename = `${id}_${originalname}`;
-  const newPath = path.join(avatarPath, filename);
-
   await paramsAvatar(oldPath);
-  await fs.rename(oldPath, newPath);
-
-  const avatarURL = path.join("avatars", filename);
+  const { url: avatarURL } = await cloudinary.uploader.upload(req.file.path, {
+    folder: "waterTracker-avatars",
+  });
+  await fs.unlink(req.file.path);
   await User.findByIdAndUpdate(id, { avatarURL });
-
   res.json({
     avatarURL,
   });
