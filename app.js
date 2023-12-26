b17cc9e31332e7d185d5355879afd2b885f4bbf2
Seven <graf.lancelot7@gmail.com>
2023-12-26 22:56:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use("/users", usersRouter);
 app.use("/api/water-track", waterTrackRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
   console.log(err);
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({
     message,
   });
