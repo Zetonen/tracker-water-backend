5c45847079282f6c9aa6d60172babf6db89f7824
Seven <graf.lancelot7@gmail.com>
2023-12-26 23:22:45 +0200

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,5 @@
-export { default as HttpError } from './HttpError.js';
-export { default as paramsAvatar } from './paramsAvatar.js';
-export { default as sendEmail } from './sendEmail.js';
-export { default as dateFormat } from './dateFormat.js';
-export { default as cloudinary } from './cloudinary.js';
+export { default as HttpError } from "./HttpError.js";
+export { default as paramsAvatar } from "./paramsAvatar.js";
+export { default as sendEmail } from "./sendEmail.js";
+export { default as dateFormat } from "./dateFormat.js";
+export { default as cloudinary } from "./cloudinary.js";
