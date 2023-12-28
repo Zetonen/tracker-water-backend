07f079dbd56f7c32683010be06526a35f4cfcb60
Seven <graf.lancelot7@gmail.com>
2023-12-28 23:04:43 +0200

    add user info in response

--- controllers/users/signin.js
+++ controllers/users/signin.js
@@ -7,22 +7,32 @@ import "dotenv/config";
 const { JWT_SECRET } = process.env;
 
 const signin = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw HttpError(401, "Email or password invalid!");
-  }
-  const comparedPassword = await bcrypt.compare(password, user.password);
-  if (!comparedPassword) {
-    throw HttpError(401, "Email or password invalid!");
-  }
-  const payload = {
-    id: user._id,
-  };
-  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(user._id, { token });
+ const { email, password } = req.body;
+ const user = await User.findOne({ email });
+ if (!user) {
+  throw HttpError(401, "Email or password invalid!");
+ }
+ const { username, gender, dailyNorma, avatarURL } = user;
+ const comparedPassword = await bcrypt.compare(password, user.password);
+ if (!comparedPassword) {
+  throw HttpError(401, "Email or password invalid!");
+ }
+ const payload = {
+  id: user._id,
+ };
+ const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+ await User.findByIdAndUpdate(user._id, { token });
 
-  res.json({ token });
+ res.json({
+  token,
+  user: {
+   email,
+   username,
+   gender,
+   dailyNorma,
+   avatarURL,
+  },
+ });
 };
 
 export default signin;
--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,30 +1,34 @@
 import bcrypt from "bcryptjs";
 import path from "path";
-import gravatar from "gravatar";
 import User from "../../model/User.js";
 import { HttpError } from "../../helpers/index.js";
 
 const defaultAvatarPath = path.resolve("public", "avatar");
 
 const signup = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (user) {
-    throw HttpError(409, "Email already exist!");
-  }
-  const avatarURL = `${defaultAvatarPath}/default_avatar.jpg`;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({
-    ...req.body,
-    password: hashedPassword,
-    avatarURL,
-  });
+ const { email, password } = req.body;
+ const user = await User.findOne({ email });
+ if (user) {
+  throw HttpError(409, "Email already exist!");
+ }
+ const avatarURL = `${defaultAvatarPath}/default_avatar.jpg`;
+ const hashedPassword = await bcrypt.hash(password, 10);
+ const newUser = await User.create({
+  ...req.body,
+  password: hashedPassword,
+  avatarURL,
+ });
+ const { username, gender, dailyNorma } = newUser;
 
-  res.status(201).json({
-    user: {
-      email: newUser.email,
-    },
-  });
+ res.status(201).json({
+  user: {
+   email,
+   username,
+   gender,
+   dailyNorma,
+   avatarURL,
+  },
+ });
 };
 
 export default signup;
