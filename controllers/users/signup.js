e1a03eb36eb1c214507d6d4f613afc98cec3ee92
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2024-01-05 22:38:45 +0200

    updated

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,31 +1,38 @@
 import bcrypt from "bcryptjs";
-import path from "path";
+import jwt from "jsonwebtoken";
 import User from "../../model/User.js";
 import { HttpError } from "../../helpers/index.js";
+import "dotenv/config";
 
+const { JWT_SECRET } = process.env;
 
 const signup = async (req, res) => {
- const { email, password } = req.body;
- const user = await User.findOne({ email });
- if (user) {
-  throw HttpError(409, "Email already exist!");
- }
- const hashedPassword = await bcrypt.hash(password, 10);
- const newUser = await User.create({
-  ...req.body,
-  password: hashedPassword,
- });
- const { username, gender, dailyNorma, avatarURL } = newUser;
-
- res.status(201).json({
-  user: {
-   email,
-   username,
-   gender,
-   dailyNorma,
-   avatarURL,
-  },
- });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already exist!");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashedPassword,
+  });
+  const { _id, username, gender, dailyNorma, avatarURL } = newUser;
+  const payload = {
+    id: _id,
+  };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(_id, { token });
+  res.status(201).json({
+    token,
+    user: {
+      email,
+      username,
+      gender,
+      dailyNorma,
+      avatarURL,
+    },
+  });
 };
 
 export default signup;
