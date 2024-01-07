3a1e069000db2223d542e91db0b3bde0df3a6580
Seven <graf.lancelot7@gmail.com>
2024-01-07 14:50:07 +0200

    Update dateFormat.js

--- helpers/dateFormat.js
+++ helpers/dateFormat.js
@@ -1,7 +1,8 @@
 import { format } from "date-fns";
 
-const dateFormat = (date) => {
+const dateFormat = date => {
   const adjustedDate = new Date(Number(date));
+  adjustedDate.setHours(adjustedDate.getHours() + 2); 
   return format(adjustedDate, "yyyy-MM-dd'T'HH:mm:ss");
 };
 export default dateFormat;
