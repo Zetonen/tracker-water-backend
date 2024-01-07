31fa39b0a66bf0de299ae97fc56601a6f6dc951e
Seven <graf.lancelot7@gmail.com>
2024-01-07 14:06:40 +0200

    fix

--- controllers/users/getInfo.js
+++ controllers/users/getInfo.js
@@ -1,7 +1,8 @@
 import { transformCreateAt } from "../../helpers/index.js";
 
 const getInfo = async (req, res) => {
-  const { username, email, gender, dailyNorma, avatarURL } = req.user;
+  const { username, email, gender, dailyNorma, avatarURL, createdAt } =
+    req.user;
   const validCreateAt = transformCreateAt(createdAt);
 
   res.json({
