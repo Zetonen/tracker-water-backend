52d77b7988443d6b0163ab187501bd1fb0a8a83e
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2024-01-07 22:47:30 +0200

    fixed swagger and changeDailyNorma updated

--- controllers/users/changeDailyNorma.js
+++ controllers/users/changeDailyNorma.js
@@ -1,8 +1,21 @@
 import User from "../../model/User.js";
+import WaterTrack from "../../model/WaterTracker.js";
 
 const changeDailyNorma = async (req, res) => {
   const { dailyNorma } = req.body;
   const { _id } = req.user;
+  const owner = _id;
+  const todayDate = new Date().toISOString().split("T")[0];
+  const waterTracks = await WaterTrack.find({ owner });
+  const todayWaterTracks = waterTracks.filter(
+    item => item.date.split("T")[0] === todayDate
+  );
+  const todayWaterAmount =
+    todayWaterTracks.reduce((acc, item) => item.amountWater + acc, 0) / 1000;
+  const percentageWaterConsumed = (
+    (todayWaterAmount / dailyNorma) *
+    100
+  ).toFixed(2);
   const result = await User.findByIdAndUpdate(
     _id,
     { dailyNorma },
@@ -13,7 +26,12 @@ const changeDailyNorma = async (req, res) => {
   );
 
   res.json({
-    dailyNorma: result.dailyNorma,
+    user: {
+      dailyNorma: result.dailyNorma,
+    },
+    today: {
+      percentageWaterConsumed: `${percentageWaterConsumed}%`,
+    },
   });
 };
 
