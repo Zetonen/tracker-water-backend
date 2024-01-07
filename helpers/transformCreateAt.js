fd5dca1125bb805b9f7eb9b6755b7bef852be47d
Seven <graf.lancelot7@gmail.com>
2024-01-07 14:14:26 +0200

    Update transformCreateAt.js

--- helpers/transformCreateAt.js
+++ helpers/transformCreateAt.js
@@ -1,8 +1,8 @@
 const transformCreateAt = at => {
-  return at.toLocaleString({}, {
-    dateStyle: "short",
-    timeStyle: "short"
-  })
-  // return at.toLocaleString().split(",")[0].split(".").reverse().join("-");
+  // return at.toLocaleString({}, {
+  //   dateStyle: "short",
+  //   timeStyle: "short"
+  // })
+  return at.toLocaleString().split(",")[0].split("/").reverse().join("-");
 };
 export default transformCreateAt;
